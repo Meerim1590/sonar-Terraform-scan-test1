bf3b94b46d0baa7e847785a0f7d4e46508c17c05
Meerim1590 <92472676+Meerim1590@users.noreply.github.com>
2024-04-15 19:07:43 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ resource “aws_security_group” “allow_tls” {
     from_port        = 80
     to_port          = 80
     protocol         = “tcp”
-    cidr_blocks      = [“0.0.0.0/0”]
+    cidr_blocks      = [“127.0.0.1/32”]
   }
   ingress {
     description      = “TLS from VPC”
